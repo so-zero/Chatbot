981742b62b7deaa1f3095910aa2c306f6cb38126
so-zero <zero_s0@naver.com>
2024-08-26 17:12:54 +0900

    chatbot page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Chatbot from "./components/Chatbot";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex justify-center items-center w-full min-h-screen bg-gray-50">
       <Chatbot />
     </div>
   );
